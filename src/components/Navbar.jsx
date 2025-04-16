b03d9122066ff47c9a0c6d35e8a270a130aa7050
LuizGustavoReis <luizgusreis@gmail.com>
2025-04-16 20:07:22 -0300

    esta pronto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,40 @@
-import styles from './Navbar.module.css'
-import { NavLink } from 'react-router-dom'
+import styles from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-
   return (
     <>
       <nav className={styles.navbar}>
         <ul className={styles.link_list}>
-          <NavLink to="/" className={styles.brand} activeClassName={styles.active}>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? `${styles.brand} ${styles.active}` : styles.brand
+            }
+          >
             <li><span>Life</span>Dev</li>
           </NavLink>
-          <NavLink to="/login" className={styles.link} activeClassName={styles.active}>
+          <NavLink
+            to="/login"
+            className={({ isActive }) =>
+              isActive ? `${styles.link} ${styles.active}` : styles.link
+            }
+          >
             <li>Login</li>
           </NavLink>
-          <NavLink to="/register" className={styles.link} activeClassName={styles.active}>
+          <NavLink
+            to="/register"
+            className={({ isActive }) =>
+              isActive ? `${styles.link} ${styles.active}` : styles.link
+            }
+          >
             <li>Register</li>
           </NavLink>
           <button className={styles.exit}>Exit</button>
         </ul>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
