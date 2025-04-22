364c17c174b5327b796237b380fac91720746d18
WillianSCorrea <willianwcorrea@gmail.com>
2025-04-22 17:36:19 -0300

    LifeDev

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,11 @@ const Navbar = () => {
     <>
       <nav className={styles.navbar}>
         <ul className={styles.links_list}>
-          <NavLink to="/" className={styles.brand} activeClassName={styles.active}>
+          <NavLink to="/" className={styles.brand} >
             <li><span>Life</span>Dev</li> </NavLink>
-          <NavLink to="/login" className={styles.link} activeClassName={styles.active}>
+          <NavLink to="/login" className={styles.link} >
             <li>Login</li> </NavLink>
-          <NavLink to="/register" className={styles.link} activeClassName={styles.active}>
+          <NavLink to="/register" className={styles.link} >
             <li>Register</li>  </NavLink>
 
           <button className={styles.exit}>Exit</button>
